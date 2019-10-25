f61e7eaf676de8622c013d4ed85942e9ec55a284
young0915 <38437784+young0915@users.noreply.github.com>
2019-10-26 00:41:01 +0900

    Add files via upload

---
+++ attack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+
+    public GameObject playerBall;
+    public Transform pos;
+   public GameObject dd;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if(Input.GetMouseButtonDown(0))
+    //    {
+    //        Instantiate(playerBall, pos.position, transform.rotation);
+    //    }
+    //}
+
+   
+}
